8d78171d6576dffdab7f8f252c1b90bd25b83993
dawsonrichey <dawsonrichey89@gmail.com>
2021-10-07 04:13:37 -0400

    3 files modified, 1 added

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-console.log('hello!')
+const moviesUrl =
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vTT3tIRabyINC1SiKuJUrqibo5XrUyOtlbwBeGpWZzNPoLbnw8PougKzkONugzr4AmfWT3_grR0XhOy/pub?gid=1784848680&single=true&output=csv';
+
+const showsUrl =
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vTT3tIRabyINC1SiKuJUrqibo5XrUyOtlbwBeGpWZzNPoLbnw8PougKzkONugzr4AmfWT3_grR0XhOy/pub?gid=1267192324&single=true&output=csv';
+
+const parksUrl =
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vTT3tIRabyINC1SiKuJUrqibo5XrUyOtlbwBeGpWZzNPoLbnw8PougKzkONugzr4AmfWT3_grR0XhOy/pub?gid=19832608&single=true&output=csv';
+
+const app = new Vue({
+  el: '#app',
+  data: function () {
+    return {
+      movies: [],
+      shows: [],
+      parks: [],
+    };
+  },
+  created: function () {
+    this.fetchMovies();
+    this.fetchShows();
+    this.fetchParks();
+  },
+  methods: {
+    fetchMovies() {
+      Papa.parse(moviesUrl, {
+        download: true,
+        header: true,
+        complete: (results) => (this.movies = results.data),
+      });
+    },
+    fetchShows() {
+      const _this = this;
+      Papa.parse(showsUrl, {
+        download: true,
+        header: true,
+        complete: function (results) {
+          _this.shows = results.data;
+        },
+      });
+    },
+    fetchParks() {
+      const _this = this;
+      Papa.parse(parksUrl, {
+        download: true,
+        header: true,
+        complete: function (results) {
+          _this.parks = results.data;
+        },
+      });
+    },
+  },
+});
